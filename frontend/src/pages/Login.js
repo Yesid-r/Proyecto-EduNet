686fa61480fab0e6ac0fa5c2d98f5e9db1e6b959
Yesid-r <dairo.rincon@uptc.edu.co>
2023-03-12 17:19:09 -0500

    login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,7 +4,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                Login sesino
+                Login sesion
             </div>
         );
     }
